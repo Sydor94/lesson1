ec40a981523d98d544c1a45d925797253668d86b
Web <web@it.step>
2018-09-22 20:40:13 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+       
+       <?php
+        echo "hello"?>
+        
+        </h1> 
+        
+    <a href="/url/"<?php echo "url"; ?></a>
+</body>
+</html>
